b14a8c4386b253e34b22ebf071a3929cfd013756
iagosm <iagosousa201486@gmail.com>
2024-06-05 22:40:34 -0300

    :construction: feat: Em processo inclusão do layout. Startando criação do projeto

---
+++ resources/views/products.blade.php
@@ -0,0 +1,4 @@
+<h1>Está é a pagina de produtos</h1>
+<a href="/">Home</a>
+<a href="/contact">Contatos</a>
+<a href="/products">Productions</a>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  $arr = [13, 22, 35, 46, 52, 61];
+  return view('welcome', ['arr' => $arr]);
+});
+
+Route::get('/contact', function () {
+  return view('contact');
+});
+
+Route::get('/products', function () {
+  return view('products');
+});
